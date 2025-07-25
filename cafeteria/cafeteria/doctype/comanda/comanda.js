0dd3fa0b40dbf645ea2255db4e1d1aa7ea33c824
FisEmiliano <oscar.bernab3@gmail.com>
2025-07-24 19:04:40 -0600

    Update comanda.js

--- cafeteria/cafeteria/doctype/comanda/comanda.js
+++ cafeteria/cafeteria/doctype/comanda/comanda.js
@@ -1,8 +1,10 @@
 // Copyright (c) 2025, Sebastian Morales and contributors
 // For license information, please see license.txt
 
-frappe.ui.form.on("Comanda", {
-	refresh(frm) {
-
- 	},
- });
+frappe.ui.form.on('Comanda', {
+    refresh: function(frm) {
+        frm.add_custom_button('Test', function() {
+            frappe.msgprint(__('Botón Test presionado'));
+        });
+    }
+});
